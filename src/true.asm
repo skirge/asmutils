915fa903f640c9facd2698c9d60b6bd46ca631a0
konst <konst>
2001-09-04 17:52:54 +0000

    *** empty log message ***

--- src/true.asm
+++ src/true.asm
@@ -1,16 +1,17 @@
 ;Copyright (C) Indrek Mandre <indrek.mandre@tallinn.ee>
 ;
-;$Id: true.asm,v 1.4 2001/01/21 15:18:46 konst Exp $
+;$Id: true.asm,v 1.5 2001/09/04 17:52:54 konst Exp $
 ;
 ;hackers' true/false
 ;
-;0.01: 17-Jun-1999	initial release
-;0.02: 04-Jul-1999	fixed bug with 2.0 kernel
-;0.03: 20-Sep-1999	size improvements
-;0.04: 05-Jan-2001	even more size improvements ;)
-;
 ;syntax: true
 ;	 false
+;
+;0.01: 17-Jun-1999	initial release
+;0.02: 04-Jul-1999	fixed bug with 2.0 kernel (KB)
+;0.03: 20-Sep-1999	size improvements (KB)
+;0.04: 05-Jan-2001	even more size improvements ;) (KB)
+;0.05  29-Aug-2001      even more, more size improvements ;)) [two bytes] (RM)
 
 %include "system.inc"
 
@@ -24,9 +25,11 @@ START:
 	or 	al,al
 	jnz	.n1
 	xor	ebx,ebx
-	cmp	byte [esi-5],'t'
-	jz	.exit
-	inc	ebx
+	shr	byte [esi-5],1
+	rcl 	ebx,1
+;	cmp	byte [esi-5],'t'
+;	jz	.exit
+;	inc	ebx
 .exit:
 	sys_exit
 
