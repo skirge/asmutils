2f7df5f8f042911e18c78bd8a22aed8642b4136a
konst <konst>
2002-03-21 08:33:21 +0000

    added mkswap extension for Linux 2.2+

--- src/swapon.asm
+++ src/swapon.asm
@@ -1,17 +1,18 @@
 ;Copyright (C) 1999 Konstantin Boldyshev <konst@linuxassembly.org>
+;Copyright (C) 2002 Thomas M. Ogrisegg <tom@rhadamanthys.org>
 ;
-;$Id: swapon.asm,v 1.2 2000/02/10 15:07:04 konst Exp $
+;$Id: swapon.asm,v 1.3 2002/03/21 08:33:21 konst Exp $
 ;
-;hackers' swapon/swapoff
-;
-;0.01: 04-Jul-1999	initial release
+;hackers' swapon/swapoff/mkswap
 ;
 ;syntax: swapon device ...
 ;
 ;example: swapon /dev/hda9 /dev/hda10
 ;	  swapoff /dev/hda5
+;	  mkswap /dev/hda8
 ;
-;quite dumb version
+;0.01: 04-Jul-1999	initial release
+;0.02: 18-Mar-2002	mkswap extnesion for Linux 2.2+ (TO)
 
 %include "system.inc"
 
@@ -28,14 +29,57 @@ START:
 	pop	ebx
 	or	ebx,ebx
 	jz	.exit
+	cmp	word [esi-7],'mk'
+	jz	.mkswap
 	cmp	word [esi-3],'ff'
 	jnz	.swapon
+
+.swapoff:
 	sys_swapoff
-	jmp short .next_file
+	jmps	.next_file
+
 .swapon:
 	sys_swapon
-	jmp short .next_file
+	jmps	.next_file
+
+.mkswap:
+	sys_open EMPTY,O_RDWR
+	test	eax,eax
+	js	.exit
+	mov	ebp,eax
+	sys_lseek ebp,0,SEEK_END
+	test	eax,eax
+	jns	.do_mkswap
+
+.error:
+	sys_write STDERR,error,errlen
 .exit:
 	sys_exit eax
+
+.do_mkswap:
+	mov	edi, eax
+	shr	edi, 0xc
+	dec	edi
+	sys_lseek ebp,0x400,SEEK_SET
+	test	eax,eax
+	js	.error
+	push	byte 0x1
+	mov	ecx,esp
+	sys_write ebp,EMPTY,4
+	push	edi
+	mov	ecx,esp
+	sys_write ebp,EMPTY,4
+	sys_lseek ebp,0xff6,SEEK_SET
+	sys_write ebp,signature,siglen
+	test	eax,eax
+	js	.error
+	sys_close ebp
+	xor	eax,eax
+	jmps	.exit
+
+signature	db	"SWAPSPACE2"
+siglen	equ	$	-	signature
+error		db	"i/o error.", __n
+errlen	equ	$	-	error
 
 END
