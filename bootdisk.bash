94b2b4364d32889fe9c2a918d44be68649391f8d
konst <konst>
2002-08-16 15:09:14 +0000

    added optional root fs path

--- bootdisk.bash
+++ bootdisk.bash
@@ -4,7 +4,7 @@
 #
 # Copyright (C) 2001	Karsten Scheibler <karsten.scheibler@bigfoot.de>
 #
-# $Id: bootdisk.bash,v 1.3 2002/03/02 20:23:14 konst Exp $
+# $Id: bootdisk.bash,v 1.4 2002/08/16 15:09:14 konst Exp $
 
 
 TMP_DIR=/tmp/bootdisk-$$
@@ -59,16 +59,26 @@ function create_rc
 cat <<EOF >"$1" 
 #!/bin/sh
 
+export TERM=linux
+export PATH=/bin:/sbin
+export HOME=/tmp
+export SHELL=/bin/sh
+
+echo -e "\033[2J\033[3;1H\t\033[1;37mWelcome to the \033[34masmutils\033[37m bootdisk! [\033[32mhttp://linuxassembly.org\033[37m]\033[0;37m\n\n\n"
+
 echo -e "mounting /proc..\c"
 mount -t proc none /proc
 && echo -e "done"
 
+hostname alpha
+
 echo -e "configuring network..\c"
 ifconfig lo 127.0.0.1
 && ifconfig eth0 192.168.0.1
-&& echo -e "done"
-
-echo -e "\033[2J\033[3;1H\t\033[1;37mWelcome to the \033[34masmutils\033[37m bootdisk! [\033[32mhttp://linuxassembly.org\033[0;37m]\n\n\n"
+&& echo -e "starting daemons..\c"
+&& httpd /etc/httpd/ 80 /etc/httpd/404.html
+&& ftpd /etc/ftpd.conf 20
+&& echo -e "done\n"
 
 EOF
 chmod 555 "$1"
@@ -81,8 +91,8 @@ fi
 
 trap 'umount "$ROOT_IMAGE" >/dev/null 2>&1; rm -rf "$TMP_DIR"' 0
 
-if [ $# -ne 2 ]; then
-	echo2 "Usage: $0 <kernelfile> <path to asmutils binaries>"
+if [ $# -le 2 ]; then
+	echo2 "Usage: $0 kernelfile path_to_asmutils_binaries [path_to_rootfs]"
 	echo2
 	echo2 "Given kernel must support ramdisk, minix fs and floppy drive."
 	error "Resulting bootdisk is written to stdout."
@@ -90,6 +100,7 @@ fi
 
 KERNEL_FILE="$1"
 ASMUTILS_PATH="$2"
+ROOT_PATH="$3"
 
 if ! mkdir "$TMP_DIR" 2>/dev/null; then
 	error "can't create '$TMP_DIR'"
@@ -108,6 +119,11 @@ echo2n "mounting root image ... "
 check2 'mount -o loop -t minix "$ROOT_IMAGE" "$MOUNT_POINT"'
 
 echo2n "copying data to root filesystem ... "
+
+if [ -d $ROOT_PATH ]; then
+    cp -a $ROOT_PATH/* $MOUNT_POINT
+else
+
 check2 '
 mkdir -m 755 "$MOUNT_POINT"/bin &&
 mkdir -m 755 "$MOUNT_POINT"/dev &&
@@ -153,9 +169,13 @@ mknod -m 600 "$MOUNT_POINT"/dev/audio c 14 4 &&
 mknod -m 600 "$MOUNT_POINT"/dev/dsp c 14 3 &&
 mknod -m 600 "$MOUNT_POINT"/dev/fb0 c 29 0 &&
 create_rc "$MOUNT_POINT/etc/rc" &&
-echo "/dev/ram0 / minix rw 0 0" > "$MOUNT_POINT"/etc/mtab &&
-( cd "$ASMUTILS_PATH"; ls -1 | while read FILE; do if [ -x "$FILE" ]; then
+echo "/dev/ram0 / minix rw 0 0" > "$MOUNT_POINT"/etc/mtab'
+fi
+
+check2 '
+  ( cd "$ASMUTILS_PATH"; ls -1 | while read FILE; do if [ -x "$FILE" ]; then
   cp -a "$FILE" "$MOUNT_POINT"/bin/; fi; done;
+  [ -n "$ADDONS_PATH" ] && cp -a -r "$ADDONS_PATH/bin" "$MOUNT_POINT";
   chown root.root "$MOUNT_POINT"/bin/*; chmod 555 "$MOUNT_POINT"/bin/* )'
 
 echo2n "copying kernel ... "
@@ -195,6 +215,9 @@ check2 'rdev -R "$BOOTDISK_IMAGE" 0'
 
 echo2n "setting kernel ramdisk word ... "
 check2 'rdev -r "$BOOTDISK_IMAGE" "$RAMDISK_WORD"'
+
+echo2n "setting kernel video mode ... "
+check2 'vidmode "$BOOTDISK_IMAGE" 768'
 
 echo2n "appending root filesystem to kernel ... "
 check2 'dd if="$ROOT_IMAGE".gz of="$BOOTDISK_IMAGE" bs=1024 seek="$KERNEL_SIZE"'
